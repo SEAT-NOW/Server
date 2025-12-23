554646d375edf65c8e6f75fb377ad6654e3c8cde
이지수 <0801ljs@naver.com>
2025-12-23 16:57:35 +0900

    docs: 스웨거 명세 추가

--- src/main/java/depth/main/seatnow/global/exception/error/ErrorResponse.java
+++ src/main/java/depth/main/seatnow/global/exception/error/ErrorResponse.java
@@ -1,12 +1,19 @@
 package depth.main.seatnow.global.exception.error;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@Schema(description = "에러 발생 시 반환되는 공통 에러 응답 객체")
 public class ErrorResponse {
+    @Schema(description = "비즈니스 에러 코드", example = "4001")
     private String code;
+
+    @Schema(description = "에러 메시지")
     private String message;
+
+    @Schema(description = "에러 상세 원인 (개발자 디버깅 및 프론트엔드 상세 처리용)")
     private String detail;
 }

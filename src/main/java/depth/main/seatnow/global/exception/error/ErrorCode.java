b7e71e2faca614fbe2e5efc52408f1893c57ada4
이지수 <0801ljs@naver.com>
2025-12-28 17:03:07 +0900

    refactor: 에러코드 번호 변경

--- src/main/java/depth/main/seatnow/global/exception/error/ErrorCode.java
+++ src/main/java/depth/main/seatnow/global/exception/error/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     EXPIRED_VERIFICATION_CODE("4003", "인증 시간이 만료되었습니다. 다시 시도해주세요."),
 
     UNAUTHORIZED("4010", "인증이 필요합니다."),
+    INVALID_TOKEN("4011", "잘못된 토큰입니다."),
 
     FORBIDDEN("4030", "접근 권한이 없습니다."),
 
@@ -20,8 +21,8 @@ public enum ErrorCode {
     CONFLICT("4090", "이미 존재하는 리소스입니다."),
 
     SERVER_ERROR("5000", "서버 내부 오류입니다."),
-    EXTERNAL_API_ERROR("5001", "외부 시스템과의 통신 중 오류가 발생했습니다."),
-    INVALID_TOKEN("4010", "잘못된 토큰입니다.");
+    EXTERNAL_API_ERROR("5001", "외부 시스템과의 통신 중 오류가 발생했습니다.");
+
 
 
     private final String code;

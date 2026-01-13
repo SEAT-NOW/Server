c1a43057b5b414b6efa49581cd03e592c1e595c9
이지수 <0801ljs@naver.com>
2026-01-13 11:16:49 +0900

    fix: 회원 탈퇴 시 DB 미반영 오류 수정 (@Transactional 추가)

--- src/main/java/depth/main/seatnow/domain/user/service/UserService.java
+++ src/main/java/depth/main/seatnow/domain/user/service/UserService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class UserService {
     private final UserRepository userRepository;
+    @Transactional
     public void withdrawUser(CustomUserDetails userDetails) {
         User user = userRepository.findById(userDetails.getUserId())
                 .orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND));
